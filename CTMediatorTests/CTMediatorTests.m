f5ddc89f517db1e1e6da7fe2bdbecf9e77dcef26
15370989688 <15370989688@163.com>
2016-12-25 16:12:32 +0800

    Initial Commit

---
+++ CTMediatorTests/CTMediatorTests.m
@@ -0,0 +1,39 @@
+//
+//  CTMediatorTests.m
+//  CTMediatorTests
+//
+//  Created by Charon on 16/12/25.
+//  Copyright © 2016年 Charon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CTMediatorTests : XCTestCase
+
+@end
+
+@implementation CTMediatorTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
